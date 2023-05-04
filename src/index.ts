07bf49f379512f7137c02a308a86a7b7163259be
Bao Huynh <hgbao1807@gmail.com>
2023-05-05 02:51:08 +0700

    feat: Implement main project and initialize requirement tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import readline from 'readline';
 
+import Processor from './Processor';
+
 const readlineInterface = readline.createInterface({
   input: process.stdin,
 });
 
+const processor = new Processor();
+
 readlineInterface.on('line', (cmd) => {
-  console.log(cmd);
+  try {
+    const response = processor.processCommand(cmd);
+    if (response) {
+      console.log(response);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
