7727c7aaee93151bb99f005c8a20af23c3322365
Bao Huynh <hgbao1807@gmail.com>
2023-05-05 01:11:38 +0700

    feat: Implement DatabaseManager

---
+++ src/DatabaseManager.spec.ts
@@ -0,0 +1,68 @@
+import DatabaseManager from './DatabaseManager';
+
+describe('DatabaseManager', () => {
+  let DbManager: DatabaseManager;
+
+  beforeEach(() => {
+    DbManager = new DatabaseManager();
+  });
+
+  it('should return NULL if get empty value', () => {
+    expect(DbManager.get('a')).toEqual('NULL');
+  });
+
+  it('should set value correctly', () => {
+    DbManager.set('a', '1807');
+    expect(DbManager.get('a')).toEqual('1807');
+  });
+
+  it('should replace value if set again', () => {
+    DbManager.set('a', '1807');
+    expect(DbManager.get('a')).toEqual('1807');
+
+    DbManager.set('a', '1808');
+    expect(DbManager.get('a')).toEqual('1808');
+  });
+
+  it('should unset value correctly', () => {
+    DbManager.set('a', '1807');
+    expect(DbManager.get('a')).toEqual('1807');
+
+    DbManager.unset('a');
+    expect(DbManager.get('a')).toEqual('NULL');
+  });
+
+  it('should be able to check if value exists', () => {
+    DbManager.set('a', '1807');
+    expect(DbManager.hasValue('1807')).toEqual('1');
+    expect(DbManager.hasValue('1808')).toEqual('0');
+
+    DbManager.set('a', '1808');
+    DbManager.set('b', '1808');
+    expect(DbManager.hasValue('1807')).toEqual('0');
+    expect(DbManager.hasValue('1808')).toEqual('1');
+
+    DbManager.unset('a');
+    expect(DbManager.hasValue('1807')).toEqual('0');
+    expect(DbManager.hasValue('1808')).toEqual('1');
+
+    DbManager.unset('b');
+    expect(DbManager.hasValue('1807')).toEqual('0');
+    expect(DbManager.hasValue('1808')).toEqual('0');
+  });
+
+  it('should copy data from parent manager if provided in constructor', () => {
+    DbManager.set('a', '1807');
+    DbManager.set('b', '1808');
+
+    const AnotherDbManager = new DatabaseManager(DbManager);
+    DbManager.unset('a');
+    DbManager.unset('b');
+
+    expect(AnotherDbManager.get('a')).toEqual('1807');
+    expect(AnotherDbManager.get('b')).toEqual('1808');
+
+    expect(AnotherDbManager.hasValue('1807')).toEqual('1');
+    expect(AnotherDbManager.hasValue('1808')).toEqual('1');
+  });
+});
